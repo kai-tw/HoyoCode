5266e8ce59d1bdd51c42d85e5163601a758d1aa9
Kai <wuvincentck505@gmail.com>
2025-01-20 02:02:04 +0800

    disable requesting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
-import { Component, ContentChildren, QueryList } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
-import { HeaderComponent } from "./header/header.component";
-import { CodeTableComponent } from "./code-table/code-table.component";
-import { TabsComponent } from "./tabs/tabs.component";
-import { TabComponent } from "./tab/tab.component";
-import { CodeData } from "../data-model/code-data";
-import { FooterComponent } from "./footer/footer.component";
-import { DisqusComponent } from "./disqus/disqus.component";
-import { HttpClient } from "@angular/common/http";
-import { LoadingStatus } from "../data-model/loading-status";
-import { GameType } from "../data-model/game-type";
-import { LoadingSpinnerComponent } from "./loading-spinner/loading-spinner.component";
+import { Component, ContentChildren, QueryList } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { CodeTableComponent } from './code-table/code-table.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { TabComponent } from './tab/tab.component';
+import { CodeData } from '../data-model/code-data';
+import { FooterComponent } from './footer/footer.component';
+import { DisqusComponent } from './disqus/disqus.component';
+import { HttpClient } from '@angular/common/http';
+import { LoadingStatus } from '../data-model/loading-status';
+import { GameType } from '../data-model/game-type';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
 
 @Component({
-  selector: "app-root",
+  selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
@@ -25,8 +25,8 @@ import { LoadingSpinnerComponent } from "./loading-spinner/loading-spinner.compo
     DisqusComponent,
     LoadingSpinnerComponent,
   ],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.sass",
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
   protected status: LoadingStatus = LoadingStatus.LOADING;
@@ -40,15 +40,15 @@ export class AppComponent {
   }
 
   ngAfterContentInit() {
-    this.httpClient
-      .get("https://codehunter-api.kai-wu.net/")
-      .subscribe((data) => {
-        this.codeList = (data as Array<Object>)
-          .map((e: any) => new CodeData(e))
-          .filter((e: CodeData) => e.isValid())
-          .sort(CodeData.compare);
-        this.status = LoadingStatus.LOADED;
-      });
+    // this.httpClient
+    //   .get("https://codehunter-api.kai-wu.net/")
+    //   .subscribe((data) => {
+    //     this.codeList = (data as Array<Object>)
+    //       .map((e: any) => new CodeData(e))
+    //       .filter((e: CodeData) => e.isValid())
+    //       .sort(CodeData.compare);
+    //     this.status = LoadingStatus.LOADED;
+    //   });
   }
 
   protected isLoaded(): Boolean {
